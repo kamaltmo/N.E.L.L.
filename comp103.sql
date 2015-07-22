9494b9eef4e6253c1a2275001fd2dda62af36dc6
Kamal Osman <kamaltmo@gmail.com>
2015-07-22 19:16:19 +0100

    Added a hint field

---
+++ comp103.sql
@@ -0,0 +1,143 @@
+-- phpMyAdmin SQL Dump
+-- version 4.3.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 22, 2015 at 08:11 PM
+-- Server version: 5.6.24
+-- PHP Version: 5.6.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `comp103`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `free_text_questions`
+--
+
+CREATE TABLE IF NOT EXISTS `free_text_questions` (
+  `question_id` int(11) NOT NULL,
+  `question` varchar(255) DEFAULT NULL,
+  `answer` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `glossary`
+--
+
+CREATE TABLE IF NOT EXISTS `glossary` (
+  `term_id` int(11) NOT NULL,
+  `term` varchar(255) DEFAULT NULL,
+  `definition` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `multi_questions`
+--
+
+CREATE TABLE IF NOT EXISTS `multi_questions` (
+  `question_id` int(11) NOT NULL,
+  `question` varchar(255) DEFAULT NULL,
+  `hint` varchar(250) NOT NULL,
+  `answer1` varchar(255) DEFAULT NULL,
+  `answer2` varchar(255) DEFAULT NULL,
+  `answer3` varchar(255) DEFAULT NULL,
+  `answer4` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `queries`
+--
+
+CREATE TABLE IF NOT EXISTS `queries` (
+  `student_id` int(11) NOT NULL,
+  `query` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE IF NOT EXISTS `students` (
+  `student_id` int(11) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `first_name` varchar(20) DEFAULT NULL,
+  `last_name` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `free_text_questions`
+--
+ALTER TABLE `free_text_questions`
+  ADD PRIMARY KEY (`question_id`);
+
+--
+-- Indexes for table `glossary`
+--
+ALTER TABLE `glossary`
+  ADD PRIMARY KEY (`term_id`);
+
+--
+-- Indexes for table `multi_questions`
+--
+ALTER TABLE `multi_questions`
+  ADD PRIMARY KEY (`question_id`);
+
+--
+-- Indexes for table `queries`
+--
+ALTER TABLE `queries`
+  ADD KEY `student_id` (`student_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `multi_questions`
+--
+ALTER TABLE `multi_questions`
+  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `queries`
+--
+ALTER TABLE `queries`
+ADD CONSTRAINT `queries_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
