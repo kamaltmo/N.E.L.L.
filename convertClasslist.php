ac406442a8234640cc1c91b2fc1a1194aa04d978
ellarowe <ella.redversrowe@gmail.com>
2015-07-23 14:20:34 +0100

    Adding class list to module
    Everything works, special characters can be included.
    
    JOB: only display modules under the control of the lecturer logged in

---
+++ convertClasslist.php
@@ -0,0 +1,144 @@
+<?php
+	/*
+		At the moment, this script will only import into the 'students' table in the database
+		It only works with an excel spreadsheet of 2 columns with:
+			*	username
+			*	student_id
+		Admin is automatically set to 0 by the Database
+		Password will be null for now
+		
+		***************************************************************************************************
+			ERRORS:
+				-	17.1:	Comma could not be identified in the 'fullName' string
+		***************************************************************************************************
+		
+		***************************************************************************************************
+			JOBS:
+				-	Only display modules the logged-in lecturer controls
+		***************************************************************************************************
+	*/
+	
+	session_start();
+	
+	define ("DB_HOST", "localhost");
+	define ("DB_USER", "root");
+	define ("DB_PASS", "");
+	define ("DB_NAME", $_SESSION["modCode"]);
+	$link = mysql_connect(DB_HOST, DB_USER, DB_PASS) or die("Couldn't make connection.");
+	$db = mysql_select_db(DB_NAME, $link) or die("Couldn't select database");
+?>
+
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Demo - Import Excel file data in mysql database using PHP, Upload Excel file data in database</title>
+		<meta name="description" content="This tutorial will learn how to import excel sheet data in mysql database using php. Here, first upload an excel sheet into your server and then click to import it into database. All column of excel sheet will store into your corrosponding database table."/>
+		<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script type="text/javascript" src="css/jquery.min.js"></script>
+        <script type="text/javascript" src="css/bootstrap.min.js"></script>
+        <link href="css/font-awesome.min.css"
+        rel="stylesheet" type="text/css">
+        <link href="css/bootstrap.css"
+        rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<div class="navbar navbar-default navbar-inverse navbar-static-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#"><span>N.E.L.L. Test</span></a>
+				</div>
+				<div class="collapse navbar-collapse" id="navbar-ex-collapse">
+					<ul class="nav navbar-nav navbar-right"></ul>
+				</div>
+			</div>
+		</div>
+		<div class="section">
+			<div class="container">
+                <div class="row">
+                    <div class="col-md-4"></div>
+                    <div class="col-md-4">
+                        <div class="well well-lg">
+							<?php
+								// This is the file path to be uploaded.
+								$inputFileName = 'classlist.htm';
+								//$myfile = fopen("classlist.htm", "r") or die("Unable to open file!");
+								
+								libxml_use_internal_errors(true);
+								// a new dom object
+								$dom = new domDocument;
+								
+								try
+								{
+									$dom->loadHTMLFile("classlist.htm");
+								} 
+								catch(Exception $e) 
+								{
+									die('Error loading file "'.pathinfo($inputFileName,PATHINFO_BASENAME).'": '.$e->getMessage());
+								}
+								
+								$dom->preserveWhiteSpace = false;
+								$firstRow = 1;
+								$rows = $dom->getElementsByTagName("tr");
+								if(!is_null($rows))
+								{
+									// Go through each row one by one
+									foreach($rows as $row)
+									{
+										if($firstRow == 0)
+										{
+											$id = $row->childNodes->item(0)->textContent;
+											$fullName = $row->childNodes->item(2)->textContent;
+											$email = $row->childNodes->item(4)->textContent;
+											$commaPosition = strpos($fullName, ',');
+											if($commaPosition === false)
+											{
+												echo '<label>error: 17.1, student ID = ' . $id . '</label>';
+											}
+											else
+											{
+												$last_name = substr($fullName, 0, $commaPosition);
+												$first_name = substr($fullName, $commaPosition + 1, strlen($fullName));
+												$query = "SELECT SUBSTRING(MD5(RAND()) FROM 1 FOR 6)";
+												$sql = mysql_query($query);
+												$recResult = mysql_fetch_array($sql);
+												$password = $recResult[0];
+												
+												//Search for duplicates
+												$query = mysql_query("SELECT student_id FROM students WHERE student_id = " . $id);
+												// If the row hasn't already been inserted create a new student user
+												if(mysql_num_rows($query) == 0) 
+												{
+													if(mysql_query("INSERT INTO students (student_id, email, password, first_name, last_name) VALUES (" . $id . ", '" . mysql_real_escape_string($email) . "', '" . mysql_real_escape_string($password) . "', '" . mysql_real_escape_string($first_name) . "', '" . mysql_real_escape_string($last_name) . "')"))
+													{
+														$msg = 'Records have been added';
+													}
+													else
+													{
+														echo '<label>Error 17.2 adding ' . $last_name . ', ' . $first_name . '</label>';
+													}
+												}
+												else
+													$msg = 'Records have been added';
+											}
+										}
+										else
+											$firstRow = 0;
+									}
+								}
+								if($msg)
+									echo '<center><label class = "control-label">' . $msg . '</label></center>';
+							?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	<body/>
+<html>
---
+++ uploadClasslist.php
@@ -0,0 +1,169 @@
+<?php
+	//***************************************************************
+	//	JOBS:
+	//		-	add lecturer login
+	//		-	only display modules belonging to logged in lecturer
+	//***************************************************************
+	
+	session_start();
+	//Lecturer login
+	
+	define ("DB_HOST", "localhost");
+	define ("DB_USER", "root");
+	define ("DB_PASS", "");
+	define ("DB_NAME", "nell");
+	$link = mysql_connect(DB_HOST, DB_USER, DB_PASS) or die("Couldn't make connection.");
+	$db = mysql_select_db(DB_NAME, $link) or die("Couldn't select database");
+	
+	$section = 0;
+    
+	$uploadStatus = 0;
+
+	//	0 - SELECT A MODULE
+	if(isset($_POST["submit1"]))
+	{
+		$_SESSION["modCode"] = $_POST["modOption"];
+		$section = 1;
+	}
+	
+	//	1 - UPLOAD CLASSLIST
+	if(isset($_POST["submit"]))
+	{
+		$section = 1;
+		if(isset($_FILES["file"]))
+		{
+			//If there was an error uploading the file
+			if($_FILES["file"]["error"] > 0)
+			{
+				echo "Return code: " . $_FILES["file"]["error"] . "<br/>";
+			}
+			// If there were no errors with the upload
+			else
+			{
+				if(file_exists($_FILES["file"]["name"]))
+				{
+					unlink($_FILES["file"]["name"]);
+				}
+				$storagename = "classlist.htm";
+				move_uploaded_file($_FILES["file"]["tmp_name"],  $storagename);
+				$uploadStatus = 1;
+			}
+		}
+		else
+		{
+			echo "No file selected <br/>";
+		}
+		
+	}
+
+	
+?>
+
+<html>
+	<head>
+		<title>Upload Excel Student List File</title>
+		<meta name="description" content="First upload the excel sheet and then click to import it into database."/>
+		<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script type="text/javascript" src="css/jquery.min.js"></script>
+        <script type="text/javascript" src="css/bootstrap.min.js"></script>
+        <link href="css/font-awesome.min.css"
+        rel="stylesheet" type="text/css">
+        <link href="css/bootstrap.css"
+        rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<div class="navbar navbar-default navbar-inverse navbar-static-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="index.php"><span>N.E.L.L. Test</span></a>
+				</div>
+				<div class="collapse navbar-collapse" id="navbar-ex-collapse">
+					<ul class="nav navbar-nav navbar-right"></ul>
+				</div>
+			</div>
+		</div>
+		<div class="section">
+			<div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                    	<ul class="nav nav-pills nav-stacked lead list-group text-info">
+                        	<li class="active"><a href="uploadExcel.php">Upload Student Information</a></li>
+                        	<li ><a href="uploadExcelQuestions.php">Upload Questions</a></li>
+                        	<li ><a href="uploadExcelGlossary.php">Upload Glossary</a></li>
+                    	</ul>
+                	</div>
+                    <div class="col-md-4">
+                        <div class="well well-lg">
+							<?php
+								switch($section)
+								{
+									case 0:
+										echo '
+											<form action="' . $_SERVER["PHP_SELF"] . '" method="post" enctype="multipart/form-data">
+												<div class = "form-group">
+													<label class = "control-label">
+														Select Module
+													</label>
+													<select name = "modOption">';
+														$query = mysql_query("SELECT mod_code FROM modules");
+														while($row = mysql_fetch_array($query))
+														{
+															echo '<option value = "' . $row["mod_code"] . '">' . $row["mod_code"] . '</option>';
+														}
+										echo '		</select>
+												</div>
+												<div class = "form-group">
+													<input type = "submit" name = "submit1" value = "Select"/>
+												</div>
+											</form>';
+										break;
+									case 1:
+										echo '
+											<form action="'. $_SERVER["PHP_SELF"].'" method="post" enctype="multipart/form-data">
+												<div class = "form-group">
+													<label class = "control-label">Excel Uploading System</label>
+												</div>';
+													if($uploadStatus==0)
+													{
+														echo '<div class = "form-group">
+															Select file
+															<input type="file" name="file" id="file" />
+														</div>
+														<div class = "form-group">
+															Submit
+															<input type="submit" name="submit" />
+														</div>';
+													}
+													if($uploadStatus==1)
+													{
+														echo 	"<center>============================= <br/><b>File Uploaded</b><br/>============================= <br/><b>Do you want to upload the data?<br/><a href='convertClasslist.php'>Click Here</a></b><br/>========================</center>";
+													}
+										echo '
+									</form>';
+								}
+								?>
+							<script type="text/javascript">
+								  var _gaq = _gaq || [];
+								  _gaq.push(['_setAccount', 'UA-38304687-1']);
+								  _gaq.push(['_trackPageview']);
+
+								  (function() {
+									var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+									ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+									var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+								  })();
+							</script>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
